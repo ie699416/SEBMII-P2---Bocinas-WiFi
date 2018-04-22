83da1b1d7b5d71c82c4c33559ea67647162be155
ie699416 <ie699416@iteso.mx>
2018-04-22 17:20:36 -0500

    Python UDP server

---
+++ UDP.py
@@ -0,0 +1,32 @@
+UDP_IP="192.168.1.100"
+UDP_PORT = 5005
+msg = chr(5)
+MESSAGE = "Hello, World!"
+Ns = 10
+
+
+binary = '{0:08b}'.format(6)
+print("binary: ", binary)
+
+print ("UDP target IP:", UDP_IP)
+print ("UDP target port:", UDP_PORT)
+print ("message:", MESSAGE)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+import wave, struct
+waveFile = wave.open('spring_HiFi.wav', 'r')
+
+2281
+length = waveFile.getnframes()
+Ms = int(length/Ns)
+Packet = ""
+print(Ms)
+
+for i in range(0,Ms):
+    sock.sendto(Packet.encode(), (UDP_IP.encode(), UDP_PORT))
+    Packet = ""
+    for j in range (0, Ns) :
+        data = struct.unpack("<h",  waveFile.readframes(1))
+        Hexa = '{0:04d}'.format(int((data[0] + 32675) * (273/4369)))
+        Packet = Packet + Hexa
+        print("binary: ", Packet)
