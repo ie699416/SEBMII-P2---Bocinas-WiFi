3e59ceff84a9c36b67878a298e0f7ca7fdc84d2f
ie699416 <ie699416@iteso.mx>
2018-04-28 22:46:03 -0500

    16ms

--- udpecho3.c
+++ udpecho3.c
@@ -1,196 +1,220 @@
 /*
- * The Clear BSD License
- * Copyright (c) 2016, Freescale Semiconductor, Inc.
- * Copyright 2016-2017 NXP
- * All rights reserved.
+ * Copyright (c) 2001-2003 Swedish Institute of Computer Science.
+ * All rights reserved. 
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
  *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission. 
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided
- *  that the following conditions are met:
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED 
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+ * SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+ * OF SUCH DAMAGE.
  *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
+ * This file is part of the lwIP TCP/IP stack.
+ * 
+ * Author: Adam Dunkels <adam@sics.se>
  *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of the copyright holder nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS LICENSE.
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/*******************************************************************************
- * Includes
- ******************************************************************************/
+#include "udpecho.h"
+
 #include "lwip/opt.h"
 
 #if LWIP_NETCONN
 
-#include "udpecho/udpecho.h"
-#include "lwip/tcpip.h"
-#include "netif/ethernet.h"
-#include "ethernetif.h"
+#include "lwip/api.h"
+#include "lwip/sys.h"
 
-#include "board.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "event_groups.h"
 
-#include "fsl_device_registers.h"
-#include "pin_mux.h"
-#include "clock_config.h"
-/*******************************************************************************
- * Definitions
- ******************************************************************************/
+#include "fsl_dac.h"
+#include "fsl_pit.h"
+#include "fsl_gpio.h"
+#include "fsl_common.h"
+#include "fsl_port.h"
 
-/* IP address configuration. */
-#define configIP_ADDR0 192
-#define configIP_ADDR1 168
-#define configIP_ADDR2 1
-#define configIP_ADDR3 100
+#include "MK64F12.h"
 
-/* Netmask configuration. */
-#define configNET_MASK0 255
-#define configNET_MASK1 255
-#define configNET_MASK2 255
-#define configNET_MASK3 0
+#define NS 100
+#define BYTES 2
 
-/* Gateway address configuration. */
-#define configGW_ADDR0 192
-#define configGW_ADDR1 168
+#define EVENT_PIT (1<<0)
+#define EVENT_PING_PONG (1<<1)
 
-#define configGW_ADDR2 1
-#define configGW_ADDR3 254
+#define PIT_FS_HANDLER PIT0_IRQHandler
+#define PIT_IRQ_ID PIT0_IRQn
+#define USEC_TO_COUNT(us, clockFreqInHz) (uint64_t)((uint64_t)us * clockFreqInHz / 1000000U)
+#define PIT_SOURCE_CLOCK CLOCK_GetFreq(kCLOCK_BusClk)
 
-/* MAC address configuration. */
-#define configMAC_ADDR {0x02, 0x12, 0x13, 0x10, 0x15, 0x11}
+EventGroupHandle_t pingPong_events;
+QueueHandle_t pingPongA_buffer;
+QueueHandle_t pingPongB_buffer;
 
-/* Address of PHY interface. */
-#define EXAMPLE_PHY_ADDRESS BOARD_ENET0_PHY_ADDRESS
+volatile bool pitIsrFlag = false;
+volatile bool pingpong = false;
+volatile bool buffer_empty = false;
 
-/* System clock name. */
-#define EXAMPLE_CLOCK_NAME kCLOCK_CoreSysClk
+uint16_t pingpong_A[NS] = { };
+uint16_t pingpong_B[NS] = { };
+uint16_t outData_x;
+uint16_t * pingpong_A_ptr = pingpong_A;
+uint16_t * pingpong_B_ptr = pingpong_A;
 
-/*! @brief Stack size of the temporary lwIP initialization thread. */
-#define INIT_THREAD_STACKSIZE 1024
+void PIT_FS_HANDLER(void) {
+	/* Clear interrupt flag.*/
+	PIT_ClearStatusFlags(PIT, 0, kPIT_TimerFlag);
 
-/*! @brief Priority of the temporary lwIP initialization thread. */
-#define INIT_THREAD_PRIO DEFAULT_THREAD_PRIO
+	if ((true != buffer_empty)) {
+		if (pingpong) {
 
-/////////////////////////////////////////////////////////////////////////////////////////////
-#include "MCG.h"
-#define CLK_FREQ_HZ 50000000  /* CLKIN0 frequency */
-#define SLOW_IRC_FREQ 32768	/*This is the approximate value for the slow irc*/
-#define FAST_IRC_FREQ 4000000 /*This is the approximate value for the fast irc*/
-#define EXTERNAL_CLOCK 0 /*It defines an external clock*/
-#define PLL_ENABLE 1 /**PLL is enabled*/
-#define PLL_DISABLE 0 /**PLL is disabled*/
-#define CRYSTAL_OSC 1  /*It defines an crystal oscillator*/
-#define LOW_POWER 0     /* Set the oscillator for low power mode */
-#define SLOW_IRC 0 		/* Set the slow IRC */
-#define CLK0_TYPE 0     /* Crystal or canned oscillator clock input */
-#define PLL0_PRDIV 25    /* PLL predivider value */
-#define PLL0_VDIV 30    /* PLL multiplier value*/
-/** Macros for debugging*/
-#define PLL_DIRECT_INIT
+			pingpong_B_ptr = pingpong_A;
+			outData_x = *pingpong_B_ptr;
+			pingpong_B_ptr++;
 
-/*******************************************************************************
- * Prototypes
- ******************************************************************************/
+		} else {
+			pingpong_A_ptr = pingpong_A;
+			outData_x = *pingpong_A_ptr;
+			pingpong_A_ptr++;
 
-/*******************************************************************************
- * Variables
- ******************************************************************************/
+		}
+	} else {
+		outData_x = 2048;
 
-/*******************************************************************************
- * Code
- ******************************************************************************/
+	}
 
-/*!
- * @brief Initializes lwIP stack.
- */
-static void stack_init(void *arg) {
-	static struct netif fsl_netif0;
-	ip4_addr_t fsl_netif0_ipaddr, fsl_netif0_netmask, fsl_netif0_gw;
-	ethernetif_config_t fsl_enet_config0 = { .phyAddress = EXAMPLE_PHY_ADDRESS,
-			.clockName = EXAMPLE_CLOCK_NAME, .macAddress = configMAC_ADDR, };
+	DAC_SetBufferValue(DAC0, 0U, (outData_x));
 
-	IP4_ADDR(&fsl_netif0_ipaddr, configIP_ADDR0, configIP_ADDR1, configIP_ADDR2,
-			configIP_ADDR3);
-	IP4_ADDR(&fsl_netif0_netmask, configNET_MASK0, configNET_MASK1,
-			configNET_MASK2, configNET_MASK3);
-	IP4_ADDR(&fsl_netif0_gw, configGW_ADDR0, configGW_ADDR1, configGW_ADDR2,
-			configGW_ADDR3);
-
-	tcpip_init(NULL, NULL);
-
-	netif_add(&fsl_netif0, &fsl_netif0_ipaddr, &fsl_netif0_netmask,
-			&fsl_netif0_gw, &fsl_enet_config0, ethernetif0_init, tcpip_input);
-	netif_set_default(&fsl_netif0);
-	netif_set_up(&fsl_netif0);
-
-	PRINTF("\r\n************************************************\r\n");
-	PRINTF(" UDP Echo example\r\n");
-	PRINTF("************************************************\r\n");
-	PRINTF(" IPv4 Address     : %u.%u.%u.%u\r\n",
-			((u8_t *) &fsl_netif0_ipaddr)[0], ((u8_t *) &fsl_netif0_ipaddr)[1],
-			((u8_t *) &fsl_netif0_ipaddr)[2], ((u8_t *) &fsl_netif0_ipaddr)[3]);
-	PRINTF(" IPv4 Subnet mask : %u.%u.%u.%u\r\n",
-			((u8_t *) &fsl_netif0_netmask)[0],
-			((u8_t *) &fsl_netif0_netmask)[1],
-			((u8_t *) &fsl_netif0_netmask)[2],
-			((u8_t *) &fsl_netif0_netmask)[3]);
-	PRINTF(" IPv4 Gateway     : %u.%u.%u.%u\r\n", ((u8_t *) &fsl_netif0_gw)[0],
-			((u8_t *) &fsl_netif0_gw)[1], ((u8_t *) &fsl_netif0_gw)[2],
-			((u8_t *) &fsl_netif0_gw)[3]);
-	PRINTF("************************************************\r\n");
-
-	udpecho_init();
-
-	vTaskDelete(NULL);
 }
 
-/*!
- * @brief Main function
- */
-int main(void) {
+static void server_thread(void *arg) {
+	struct netconn *conn;
+	struct netbuf *buf;
 
-	/*********************************************************************************************
-	 * System clock upping to 60 MHz
-	 *********************************************************************************************/
-	uint32_t mcg_clk_hz;
-	unsigned char modeMCG = 0;
+	uint8_t *msg;
+	uint8_t UDP_buffer[BYTES * NS] = { };
+	uint16_t DAC_buffer[NS];
+	uint16_t * DAC_value;
 
-	mcg_clk_hz = pll_init(CLK_FREQ_HZ, LOW_POWER, EXTERNAL_CLOCK, PLL0_PRDIV,
-	PLL0_VDIV, PLL_ENABLE);
-	/*********************************************************************************************/
+	uint16_t buffer_index = 0;
+	uint16_t len;
 
-	SYSMPU_Type *base = SYSMPU;
-	BOARD_InitPins();
-	BOARD_BootClockRUN();
-	BOARD_InitDebugConsole();
-	/* Disable SYSMPU. */
-	base->CESR &= ~SYSMPU_CESR_VLD_MASK;
+	LWIP_UNUSED_ARG(arg);
+	conn = netconn_new(NETCONN_UDP);
+	netconn_bind(conn, IP_ADDR_ANY, 50005);
+	//LWIP_ERROR("udpecho: invalid conn", (conn != NULL), return;);
 
-	/* Initialize lwIP from thread */
-	if (sys_thread_new("main", stack_init, NULL, INIT_THREAD_STACKSIZE,
-			INIT_THREAD_PRIO) == NULL)
-		LWIP_ASSERT("main(): Task creation failed.", 0);
+	while (1) {
 
-	vTaskStartScheduler();
+		netconn_recv(conn, &buf);
 
-	/* Will not get here unless a task calls vTaskEndScheduler ()*/
-	return 0;
+		if (ERR_OK == netbuf_data(buf, (void**) &msg, &len)) {
+
+			GPIO_PinWrite(GPIOA, 1, 1);
+
+			if (pingpong) {
+
+				for (buffer_index = 0; buffer_index < NS; buffer_index++) {
+					DAC_value = msg;
+					pingpong_A[buffer_index] = *DAC_value;
+					msg++;
+					msg++;
+					DAC_value++;
+				}
+
+			} else {
+
+				for (buffer_index = 0; buffer_index < NS; buffer_index++) {
+					DAC_value = msg;
+					pingpong_B[buffer_index] = *DAC_value;
+					msg++;
+					msg++;
+					DAC_value++;
+				}
+			}
+
+			pingpong = ~pingpong;
+
+		}
+
+		GPIO_PinWrite(GPIOA, 1, 0);
+
+		buffer_index = 0;
+
+		netbuf_delete(buf);
+	}
+
 }
-#endif
+
+/*-----------------------------------------------------------------------------------*/
+void udpecho_init(void) {
+
+	CLOCK_EnableClock(kCLOCK_PortA);
+
+	port_pin_config_t config_switch = { kPORT_PullUp, kPORT_SlowSlewRate,
+			kPORT_PassiveFilterDisable, kPORT_OpenDrainDisable,
+			kPORT_LowDriveStrength, kPORT_MuxAsGpio, kPORT_UnlockRegister };
+
+	PORT_SetPinConfig(PORTA, 1, &config_switch);
+	gpio_pin_config_t switch_config_gpio = { kGPIO_DigitalOutput, 1 };
+	GPIO_PinInit(GPIOA, 1, &switch_config_gpio);
+
+	dac_config_t dacConfigStruct;
+	pit_config_t pitConfig;
+
+	DAC_GetDefaultConfig(&dacConfigStruct);
+	DAC_Init(DAC0, &dacConfigStruct);
+	DAC_Enable(DAC0, true); /* Enable output. */
+
+	PIT_GetDefaultConfig(&pitConfig);
+	PIT_Init(PIT, &pitConfig);
+	PIT_EnableInterrupts(PIT, kPIT_Chnl_0, kPIT_TimerInterruptEnable);
+	EnableIRQ(PIT_IRQ_ID);
+	PIT_SetTimerPeriod(PIT, 0, USEC_TO_COUNT(35U, PIT_SOURCE_CLOCK));
+
+	sys_thread_new("server", server_thread, NULL, 600, configMAX_PRIORITIES);
+	GPIO_PinWrite(GPIOA, 1, 1);
+
+	if ((true != buffer_empty)) {
+		if (pingpong) {
+
+			pingpong_B_ptr = pingpong_A;
+			outData_x = *pingpong_B_ptr;
+			pingpong_B_ptr++;
+
+		} else {
+			pingpong_A_ptr = pingpong_A;
+			outData_x = *pingpong_A_ptr;
+			pingpong_A_ptr++;
+
+		}
+	} else {
+		outData_x = 2048;
+
+	}
+
+	DAC_SetBufferValue(DAC0, 0U, (outData_x));
+
+	GPIO_PinWrite(GPIOA, 1, 0);
+
+	PIT_StartTimer(PIT, kPIT_Chnl_0);
+
+}
+
+#endif /* LWIP_NETCONN */
