9035417c02e9d745c260187bc287ddb2f5bb0059
ie699416 <ie699416@iteso.mx>
2018-05-01 01:57:56 -0500

    44100

--- UDP.py
+++ UDP.py
@@ -5,15 +5,17 @@ import time
 UDP_IP="192.168.1.100"
 UDP_PORT = 50005
 Ms = 0
-Ns = 2000
+Ns = 1740
 Ns_index = 0
 Ms_index = 0
 
 print ("UDP target IP:", UDP_IP)
 print ("UDP target port:", UDP_PORT)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
-buffer, fs = sf.read('september.wav', Ns, Ms_index, None, 'int16')
-bufferAux, fs = sf.read('september.wav', 500, Ms_index, None, 'int16')
+buffer, fs = sf.read('spring_HiFi.wav', Ns, Ms_index, None, 'int16')
+bufferAux, fs = sf.read('spring_HiFi.wav', 1700, Ms_index, None, 'int16')
+
+start = time.time()
 
 
 while (len(buffer)) == Ns:
@@ -23,16 +25,19 @@ while (len(buffer)) == Ns:
         buffer[Ns_index] = (buffer[Ns_index] + 32675) * (273/4369)
 
 
-    for Ns_index in range (0, int(Ns/4)):
-        bufferAux[Ns_index] = buffer[(Ns_index * 4)]
+    #for Ns_index in range (0, int(Ns/2)):
+    #    bufferAux[Ns_index] = buffer[(Ns_index * 2)]
 
 
     Ms_index = Ms_index + Ns
-    start = time.time()
-    sock.sendto(bufferAux, (UDP_IP.encode(), UDP_PORT))
-    #time.sleep(0.01)
-    print(bufferAux)
+
+    #print(bufferAux)
+    sock.sendto(buffer, (UDP_IP.encode(), UDP_PORT))
     end = time.time()
     process = end - start
+    start = time.time()
+    if process != 0:
+        print(process)
+    # time.sleep(0.01)
 
-    buffer, fs = sf.read('september.wav', Ns, Ms_index, None, 'int16')
+    buffer, fs = sf.read('spring_HiFi.wav', Ns, Ms_index, None, 'int16')
