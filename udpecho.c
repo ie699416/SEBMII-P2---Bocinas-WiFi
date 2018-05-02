49ed035a13922a922df39c471da1daf4959cf1c9
ie699416 <ie699416@iteso.mx>
2018-05-01 23:12:39 -0500

    TCP/UDP

--- udpecho.c
+++ udpecho.c
@@ -36,8 +36,12 @@
 
 #if LWIP_NETCONN
 
-#include "lwip/api.h"
 #include "lwip/sys.h"
+#include "lwip/api.h"
+
+#define OPTION_ONE 49
+#define OPTION_TWO 50
+#define OPTION_TREE 51
 
 #include "FreeRTOS.h"
 #include "task.h"
@@ -53,12 +57,16 @@
 
 #include "MK64F12.h"
 
-#define NS 430 		// MAX 540
+#define NS 700 		// MAX 540
 #define BYTES 2
 
 #define EVENT_PIT (1<<0)
 #define EVENT_PING_PONG (1<<1)
 #define EVENT_MAX_COUNT (1<<2)
+#define EVENT_BIND (1<<3)
+
+#define EVENT_PLAY_PAUSE (1<<1)
+#define EVENT_SWITCH_SONG (1<<2)
 
 #define PIT_FS_HANDLER PIT0_IRQHandler
 #define PIT_IRQ_ID PIT0_IRQn
@@ -66,17 +74,16 @@
 #define PIT_SOURCE_CLOCK CLOCK_GetFreq(kCLOCK_BusClk)
 
 EventGroupHandle_t pingPong_events;
-QueueHandle_t pingPongA_buffer;
-QueueHandle_t pingPongB_buffer;
-SemaphoreHandle_t PIT_semaphore;
+EventGroupHandle_t tcp_events;
 
-uint16_t pingpong_A[NS] = { };
-uint16_t pingpong_B[NS] = { };
 uint16_t outData_x;
-uint16_t * pingpong_A_ptr = pingpong_A;
-uint16_t * pingpong_B_ptr = pingpong_A;
+
+int16_t * pingpong_A_ptr;
+int16_t * pingpong_B_ptr;
 uint16_t pingpongA_semaphore;
 uint16_t pingpongB_semaphore;
+
+struct netconn *connUDP;
 
 void PIT_FS_HANDLER(void) {
 	/* Clear interrupt flag.*/
@@ -86,10 +93,9 @@ void PIT_FS_HANDLER(void) {
 
 		if (NS == pingpongB_semaphore) {
 			outData_x = 2048;
-			DAC_SetBufferValue(DAC0, 0U, (outData_x));
 
 		} else {
-			outData_x = *pingpong_B_ptr;
+			outData_x = (uint16_t) ((*pingpong_B_ptr) + 32675) * (273.0 / 4369);
 			pingpong_B_ptr++;
 			pingpongB_semaphore++;
 
@@ -97,10 +103,9 @@ void PIT_FS_HANDLER(void) {
 	} else {
 		if (NS == pingpongA_semaphore) {
 			outData_x = 2048;
-			DAC_SetBufferValue(DAC0, 0U, (outData_x));
 
 		} else {
-			outData_x = *pingpong_A_ptr;
+			outData_x = (uint16_t) ((*pingpong_A_ptr) + 32675) * (273.0 / 4369);
 			pingpong_A_ptr++;
 			pingpongA_semaphore++;
 
@@ -112,28 +117,131 @@ void PIT_FS_HANDLER(void) {
 }
 
 /*-----------------------------------------------------------------------------------*/
+static void tcpecho_thread(void *arg) {
+	struct netconn *conn, *newconn;
+	err_t err;
+	LWIP_UNUSED_ARG(arg);
+
+	/* Create a new connection identifier. */
+	/* Bind connection to well known port number 7. */
+#if LWIP_IPV6
+	conn = netconn_new(NETCONN_TCP_IPV6);
+	netconn_bind(conn, IP6_ADDR_ANY, 7);
+#else /* LWIP_IPV6 */
+	conn = netconn_new(NETCONN_TCP);
+	netconn_bind(conn, IP_ADDR_ANY, 7);
+#endif /* LWIP_IPV6 */
+	LWIP_ERROR("tcpecho: invalid conn", (conn != NULL), return;);
+
+	/* Tell connection to go into listening mode. */
+	netconn_listen(conn);
+
+	xEventGroupSetBits(pingPong_events, EVENT_BIND);
+
+	struct netbuf *buf;
+	void *data;
+	void *menu = "1) Detener/Reproducir audio\r\n"
+			"2) Seleccionar audio\r\n"
+			"3) Desplegar estadisticas de la comunicacion\0\r\n\n"
+			"\tEliga una opcion porfavor";
+	void *opcion1String = "Detener audio (1) /Reproducir audio (2)\n\r "
+			"eliga opcion (1) | (2):";
+	void *opcion2String = "Ingrese puerto de comunicacion: \n\r";
+	void *opcion3String = "paquetes UDP/seg  = \n\r"
+			"calidad de la com = %\n\r";
+	u16_t len;
+
+	char getCharTCP[5];
+
+	while (1) {
+
+		/* Grab new connection. */
+		err = netconn_accept(conn, &newconn);
+		/*printf("accepted new connection %p\n", newconn);*/
+		/* Process the new connection. */
+		if (err == ERR_OK) {
+
+			netconn_write(newconn, menu, 125, NETCONN_COPY);
+			while ((err = netconn_recv(newconn, &buf)) == ERR_OK) {
+				/*printf("Recved\n");*/
+				do {
+					netbuf_data(buf, &data, &len);
+					netbuf_copy(buf, getCharTCP, 5);
+
+					if (getCharTCP[0] == OPTION_ONE) {
+						netconn_write(newconn, opcion1String, 65, NETCONN_COPY);
+
+						if (EVENT_PLAY_PAUSE & xEventGroupGetBits(tcp_events)) {
+
+							DAC_Enable(DAC0, true);
+
+							xEventGroupClearBits(tcp_events,
+							EVENT_PLAY_PAUSE);
+
+						} else {
+
+							DAC_Enable(DAC0, false);
+
+							xEventGroupSetBits(tcp_events,
+							EVENT_PLAY_PAUSE);
+						}
+
+					} else if (getCharTCP[0] == OPTION_TWO) {
+						netconn_write(newconn, opcion2String, 60, NETCONN_COPY);
+						netconn_write(newconn, menu, 125, NETCONN_COPY);
+
+						if (EVENT_SWITCH_SONG & xEventGroupGetBits(tcp_events)) {
+
+							netconn_bind(connUDP, IP4_ADDR_ANY, 50005);
+
+							xEventGroupClearBits(tcp_events, EVENT_SWITCH_SONG);
+
+						} else {
+
+							netconn_bind(connUDP, IP4_ADDR_ANY, 50007);
+
+							xEventGroupSetBits(tcp_events, EVENT_SWITCH_SONG);
+						}
+					} else if (getCharTCP[0] == OPTION_TREE) {
+						netconn_write(newconn, opcion3String, 65, NETCONN_COPY);
+					}
+//             err = netconn_write(newconn, data, len, NETCONN_COPY);//echo
+#if 0
+					if (err != ERR_OK) {
+						printf("tcpecho: netconn_write: error \"%s\"\n", lwip_strerr(err));
+					}
+#endif
+				} while (netbuf_next(buf) >= 0);
+				netbuf_delete(buf);
+			}
+			/*printf("Got EOF, looping\n");*/
+			/* Close connection and discard connection identifier. */
+			netconn_close(newconn);
+			netconn_delete(newconn);
+		}
+	}
+}
+
+/*-----------------------------------------------------------------------------------*/
 static void udpecho_thread(void *arg) {
 
-	struct netconn *conn;
+	xEventGroupWaitBits(pingPong_events, EVENT_BIND, pdTRUE, pdFALSE,
+	portMAX_DELAY);
+
 	struct netbuf *buf;
 	char buffer[4096];
 	err_t err;
 
-	uint8_t *msg;
-	uint8_t UDP_buffer[BYTES * NS] = { };
-	uint16_t DAC_buffer[NS];
-	uint16_t * DAC_value;
-
-	uint16_t buffer_index = 0;
-	uint16_t len;
+	ip_addr_t dst_ip;
+	IP4_ADDR(&dst_ip, 192, 168, 1, 65);
 
 	LWIP_UNUSED_ARG(arg);
-	conn = netconn_new(NETCONN_UDP);
-	netconn_bind(conn, IP_ADDR_ANY, 50005);
+	connUDP = netconn_new(NETCONN_UDP);
+	netconn_bind(connUDP, IP4_ADDR_ANY, 50005);
 	//LWIP_ERROR("udpecho: invalid conn", (conn != NULL), return;);
 
 	while (1) {
-		err = netconn_recv(conn, &buf);
+		err = netconn_recv(connUDP, &buf);
 
 		GPIO_WritePinOutput(GPIOA, 1, 1);
 
@@ -162,7 +270,6 @@ static void udpecho_thread(void *arg) {
 			}
 
 		}
-		buffer_index = 0;
 
 		GPIO_WritePinOutput(GPIOA, 1, 0);
 		netbuf_delete(buf);
@@ -188,14 +295,13 @@ void udpecho_init(void) {
 	pit_config_t pitConfig;
 
 	pingPong_events = xEventGroupCreate();
+	tcp_events = xEventGroupCreate();
 
 	DAC_GetDefaultConfig(&dacConfigStruct);
 	DAC_Init(DAC0, &dacConfigStruct);
 	DAC_Enable(DAC0, true); /* Enable output. */
 
 	PIT_GetDefaultConfig(&pitConfig);
-
-	PIT_semaphore = xSemaphoreCreateBinary();
 
 	PIT_Init(PIT, &pitConfig);
 
@@ -209,8 +315,11 @@ void udpecho_init(void) {
 
 	PIT_SetTimerPeriod(PIT, 0, USEC_TO_COUNT(23U, PIT_SOURCE_CLOCK));
 
-	sys_thread_new("udpecho_thread", udpecho_thread, NULL,
+	sys_thread_new("tcpecho_thread", tcpecho_thread, NULL,
 	DEFAULT_THREAD_STACKSIZE, DEFAULT_THREAD_PRIO);
+
+	sys_thread_new("udpecho_thread", udpecho_thread, NULL,
+	DEFAULT_THREAD_STACKSIZE, DEFAULT_THREAD_PRIO - 1);
 
 	PIT_StartTimer(PIT, kPIT_Chnl_0);
 
